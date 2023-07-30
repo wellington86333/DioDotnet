e8ae54fccf51c98fcae78633464668dbdfc1fd5b
Wellington Brito <71724775+wellington86333@users.noreply.github.com>
2023-07-30 19:50:09 -0300

    Aprendendo C#

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using DioDotnet.models;
+
+
+    string apresentacao = "Olá, seja bem vindo";
+    int quantidade = 1;
+    double altura = 1.80;
+
+    decimal preco = 1.80M;
+
+    Console.WriteLine("Valor da variável apresentação:" + apresentacao);
+    Console.WriteLine("Valor da variável quantidade: " + quantidade);
+    Console.WriteLine("Valor da variável altura: " + altura);
+
+    Console.WriteLine("Valor da variável preço " + preco);
+
+  
+      
+      
+      
+      
+      //  Pessoa pessoa1 = new Pessoa();
+
+        //pessoa1.Nome = "Buta";
+        //pessoa1.Idade = 26;
+        //pessoa1.Apresentar();
+
