92b226d461f384c60e0f1c242a988a1d1b255194
Wellington Brito <71724775+wellington86333@users.noreply.github.com>
2023-08-01 21:42:36 -0300

    Aprendendo C#

--- Program.cs
+++ Program.cs
@@ -1,33 +1,131 @@
-﻿using DioDotnet.models;
+﻿
+
+int quantidadeEmEstoque = 3;
+
+int quantidadeCompra = 4;
+
+bool possivelVenda = quantidadeEmEstoque >= quantidadeCompra;
 
 
-    string apresentacao = "Olá, seja bem vindo";
-    int quantidade = 1;
-    quantidade = 10;
-    double altura = 1.80;
-
-    decimal preco = 1.80M;
-
-    bool condicao = true;
-
-    Console.WriteLine("Valor da variável apresentação:" + apresentacao);
-    Console.WriteLine("Valor da variável quantidade: " + quantidade);
-    Console.WriteLine("Valor da variável altura: " + altura.ToString("0.00"));
-
-    Console.WriteLine("Valor da variável preço " + preco);
-    Console.WriteLine("Valor da variável condicao: " + condicao);
+Console.WriteLine($"Quantidad em estoque: {quantidadeEmEstoque}");
+Console.WriteLine($"Quantidad compra: {quantidadeCompra}");
+Console.WriteLine($"É possível realizar a venda? {possivelVenda}");
 
 
-   
 
-  
-      
-      
-      
-      
-      //  Pessoa pessoa1 = new Pessoa();
+if (possivelVenda){
+    Console.WriteLine("Venda realizada");
 
-        //pessoa1.Nome = "Buta";
-        //pessoa1.Idade = 26;
-        //pessoa1.Apresentar();
+} else{
+    Console.WriteLine("Desculpe, não temos a quantidade desejada em estoque");
+}
+
+
+
+
+
+// double a  = 4 /(2 + 2);
+
+// Console.WriteLine(a);
+
+
+
+
+
+        // int a = int.MaxValue;
+
+        // long b = a;
+
+        // Console.WriteLine(b);
+
+        
+
+
+
+
+// int inteiro = 5;
+// string a = inteiro.ToString();
+
+// Console.WriteLine(a);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Cast - Casting 
+
+//int a =  Convert.ToInt32(null);
+
+
+//int a = int.Parse(null);
+
+
+
+//Console.WriteLine(a);
+// int a = 10 ;
+// int b = 20;
+
+// int c = a+b;
+// //c = c + 5;
+// c += 5;
+// Console.WriteLine(c);
+
+
+// string apresentacao = "Olá, seja bem vindo";
+// int quantidade = 1;
+// quantidade = 10;
+// const double altura = 1.80;
+
+// decimal preco = 1.80M;
+
+// bool condicao = true;
+
+// Console.WriteLine("Valor da variável apresentação:" + apresentacao);
+// Console.WriteLine("Valor da variável quantidade: " + quantidade);
+// Console.WriteLine("Valor da variável altura: " + altura.ToString("0.00"));
+
+
+// Console.WriteLine("Valor da variável preço " + preco);
+// Console.WriteLine("Valor da variável condicao: " + condicao);
+
+
+// DateTime dataAtual = DateTime.Now.AddDays(5);
+//Console.WriteLine(dataAtual);
+//Console.WriteLine(dataAtual.ToString("dd/MM"));
+//Console.WriteLine(dataAtual.ToString("dd/MM/YYYY/HH:mm:ss"));
+
+
+
+
+
+
+
+
+
+
+
+
+//  Pessoa pessoa1 = new Pessoa();
+
+//pessoa1.Nome = "Buta";
+//pessoa1.Idade = 26;
+//pessoa1.Apresentar();
 
--- models/Pessoa.cs
+++ models/Pessoa.cs
@@ -1,19 +1,19 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Threading.Tasks;
 
-namespace DioDotnet.models
-{
-    public class Pessoa
-    {
-        public string Nome { get; set; }
-        public int Idade { get; set; }
+// namespace DioDotnet.models
+// {
+//     public class Pessoa
+//     {
+//         public string Nome { get; set; }
+//         public int Idade { get; set; }
 
 
-        public void Apresentar()
-        {
-            Console.WriteLine($"Olá, meu nome é {Nome}, e tenho {Idade} anos");
-        }
-    }
-}
+//         public void Apresentar()
+//         {
+//             Console.WriteLine($"Olá, meu nome é {Nome}, e tenho {Idade} anos");
+//         }
+//     }
+// }
