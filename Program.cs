13ee51e1cedd5bce11e0cabd8250c3e6100da562
Wellington Brito <71724775+wellington86333@users.noreply.github.com>
2023-08-01 23:27:08 -0300

    Aprendendo switch case

--- Program.cs
+++ Program.cs
@@ -1,66 +1,86 @@
 ﻿
+using System.Collections;
 
-int quantidadeEmEstoque = 3;
+Console.WriteLine("Digite uma letra");
 
-int quantidadeCompra = 4;
-
-bool possivelVenda = quantidadeEmEstoque >= quantidadeCompra;
-
-
-Console.WriteLine($"Quantidad em estoque: {quantidadeEmEstoque}");
-Console.WriteLine($"Quantidad compra: {quantidadeCompra}");
-Console.WriteLine($"É possível realizar a venda? {possivelVenda}");
+string letra = Console.ReadLine();
 
 
+switch (letra)
+{
+    case "a":
+  
+     case "e":
+    
+     case "o":
+    
+     case "u":
+    Console.WriteLine("vogal");
+    break;
 
-if (possivelVenda){
-    Console.WriteLine("Venda realizada");
-
-} else{
-    Console.WriteLine("Desculpe, não temos a quantidade desejada em estoque");
+    
+    default: Console.WriteLine("Não é uma vogal");
+    break;
 }
 
 
 
 
 
-// double a  = 4 /(2 + 2);
 
-// Console.WriteLine(a);
+// int quantidadeEmEstoque = 3;
 
+// int quantidadeCompra = 0;
 
+// bool possivelVenda = quantidadeCompra > 0 && quantidadeEmEstoque >= quantidadeCompra;
 
 
+// Console.WriteLine($"Quantidad em estoque: {quantidadeEmEstoque}");
+// Console.WriteLine($"Quantidad compra: {quantidadeCompra}");
+// Console.WriteLine($"É possível realizar a venda? {possivelVenda}");
 
-        // int a = int.MaxValue;
+// if (quantidadeCompra == 0)
+// {
+//     Console.WriteLine("Venda inválida");
+// }
 
-        // long b = a;
+// else if (possivelVenda)
+// {
+//     Console.WriteLine("Venda realizada");
 
-        // Console.WriteLine(b);
+// }
+// else
+// {
+//     Console.WriteLine("Desculpe, não temos a quantidade desejada em estoque");
+// }
 
-        
 
 
 
 
-// int inteiro = 5;
-// string a = inteiro.ToString();
+// // double a  = 4 /(2 + 2);
 
-// Console.WriteLine(a);
+// // Console.WriteLine(a);
 
 
 
 
 
+// // int a = int.MaxValue;
 
+// // long b = a;
 
+// // Console.WriteLine(b);
 
 
 
 
 
 
+// // int inteiro = 5;
+// // string a = inteiro.ToString();
 
+// // Console.WriteLine(a);
 
 
 
@@ -70,62 +90,76 @@ if (possivelVenda){
 
 
 
-// Cast - Casting 
 
-//int a =  Convert.ToInt32(null);
 
 
-//int a = int.Parse(null);
 
 
 
-//Console.WriteLine(a);
-// int a = 10 ;
-// int b = 20;
 
-// int c = a+b;
-// //c = c + 5;
-// c += 5;
-// Console.WriteLine(c);
 
 
-// string apresentacao = "Olá, seja bem vindo";
-// int quantidade = 1;
-// quantidade = 10;
-// const double altura = 1.80;
 
-// decimal preco = 1.80M;
 
-// bool condicao = true;
 
-// Console.WriteLine("Valor da variável apresentação:" + apresentacao);
-// Console.WriteLine("Valor da variável quantidade: " + quantidade);
-// Console.WriteLine("Valor da variável altura: " + altura.ToString("0.00"));
 
 
-// Console.WriteLine("Valor da variável preço " + preco);
-// Console.WriteLine("Valor da variável condicao: " + condicao);
+// // Cast - Casting 
 
+// //int a =  Convert.ToInt32(null);
 
-// DateTime dataAtual = DateTime.Now.AddDays(5);
-//Console.WriteLine(dataAtual);
-//Console.WriteLine(dataAtual.ToString("dd/MM"));
-//Console.WriteLine(dataAtual.ToString("dd/MM/YYYY/HH:mm:ss"));
 
+// //int a = int.Parse(null);
 
 
 
+// //Console.WriteLine(a);
+// // int a = 10 ;
+// // int b = 20;
 
+// // int c = a+b;
+// // //c = c + 5;
+// // c += 5;
+// // Console.WriteLine(c);
 
 
+// // string apresentacao = "Olá, seja bem vindo";
+// // int quantidade = 1;
+// // quantidade = 10;
+// // const double altura = 1.80;
 
+// // decimal preco = 1.80M;
 
+// // bool condicao = true;
 
+// // Console.WriteLine("Valor da variável apresentação:" + apresentacao);
+// // Console.WriteLine("Valor da variável quantidade: " + quantidade);
+// // Console.WriteLine("Valor da variável altura: " + altura.ToString("0.00"));
 
 
-//  Pessoa pessoa1 = new Pessoa();
+// // Console.WriteLine("Valor da variável preço " + preco);
+// // Console.WriteLine("Valor da variável condicao: " + condicao);
 
-//pessoa1.Nome = "Buta";
-//pessoa1.Idade = 26;
-//pessoa1.Apresentar();
+
+// // DateTime dataAtual = DateTime.Now.AddDays(5);
+// //Console.WriteLine(dataAtual);
+// //Console.WriteLine(dataAtual.ToString("dd/MM"));
+// //Console.WriteLine(dataAtual.ToString("dd/MM/YYYY/HH:mm:ss"));
+
+
+
+
+
+
+
+
+
+
+
+
+// //  Pessoa pessoa1 = new Pessoa();
+
+// //pessoa1.Nome = "Buta";
+// //pessoa1.Idade = 26;
+// //pessoa1.Apresentar();
 
