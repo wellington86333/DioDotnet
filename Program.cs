22bb39c0ee6f35cca0b811b3e7c0a7f71c2e12b3
Wellington Brito <71724775+wellington86333@users.noreply.github.com>
2023-08-03 00:26:00 -0300

    Construindo calculadora

--- Program.cs
+++ Program.cs
@@ -1,165 +1,240 @@
 ﻿
-using System.Collections;
+using DioDotnet.models;
 
-Console.WriteLine("Digite uma letra");
+Calculadora calc1 = new Calculadora();
 
-string letra = Console.ReadLine();
+calc1.Somar(10, 3);
 
+calc1.Subtrair(15, 6);
 
-switch (letra)
-{
-    case "a":
-  
-     case "e":
-    
-     case "o":
-    
-     case "u":
-    Console.WriteLine("vogal");
-    break;
+calc1.Multiplicar(13, 5);
 
-    
-    default: Console.WriteLine("Não é uma vogal");
-    break;
-}
+calc1.Dividir(160,3);
 
 
 
+// bool choveu = true;
 
+// bool estaTarde = true;
 
-
-// int quantidadeEmEstoque = 3;
-
-// int quantidadeCompra = 0;
-
-// bool possivelVenda = quantidadeCompra > 0 && quantidadeEmEstoque >= quantidadeCompra;
-
-
-// Console.WriteLine($"Quantidad em estoque: {quantidadeEmEstoque}");
-// Console.WriteLine($"Quantidad compra: {quantidadeCompra}");
-// Console.WriteLine($"É possível realizar a venda? {possivelVenda}");
-
-// if (quantidadeCompra == 0)
+// if (!choveu && !estaTarde)
 // {
-//     Console.WriteLine("Venda inválida");
-// }
-
-// else if (possivelVenda)
-// {
-//     Console.WriteLine("Venda realizada");
-
+//     Console.WriteLine("Vou pedalar");
 // }
 // else
 // {
-//     Console.WriteLine("Desculpe, não temos a quantidade desejada em estoque");
+//     Console.WriteLine("Vou pedalar outro dia");
 // }
 
 
 
 
 
-// // double a  = 4 /(2 + 2);
 
-// // Console.WriteLine(a);
 
 
 
 
 
-// // int a = int.MaxValue;
 
-// // long b = a;
 
-// // Console.WriteLine(b);
 
 
 
 
 
 
-// // int inteiro = 5;
-// // string a = inteiro.ToString();
 
-// // Console.WriteLine(a);
+// // bool possuiPresencaMinima = true;
 
+// // double media = 6.5;
 
+// // if (possuiPresencaMinima && media >= 7)
+// // { Console.WriteLine("Você foi aprovado!!");
+    
+// // }
+// // else{
+// //     Console.WriteLine("Reprovado");
+// // }
 
 
+// // // bool ehMaiorDeIdade = false;
+// // // bool possuiAutorizacaoDoResponsavel = false;
 
 
+// // // if(ehMaiorDeIdade || possuiAutorizacaoDoResponsavel){
+// // //     Console.WriteLine("Entrada liberada");
 
+// // // }
 
+// // // else{
+// // //     Console.WriteLine("Entrada não liberada");
+// // // }
 
 
 
 
+// // // // using System.Collections;
 
+// // // // Console.WriteLine("Digite uma letra");
 
+// // // // string letra = Console.ReadLine();
 
 
+// // // // switch (letra)
+// // // // {
+// // // //     case "a":
+  
+// // // //      case "e":
+    
+// // // //      case "o":
+    
+// // // //      case "u":
+// // // //     Console.WriteLine("vogal");
+// // // //     break;
 
+    
+// // // //     default: Console.WriteLine("Não é uma vogal");
+// // // //     break;
+// // // // }
 
 
 
 
 
 
-// // Cast - Casting 
+// // // // // int quantidadeEmEstoque = 3;
 
-// //int a =  Convert.ToInt32(null);
+// // // // // int quantidadeCompra = 0;
 
+// // // // // bool possivelVenda = quantidadeCompra > 0 && quantidadeEmEstoque >= quantidadeCompra;
 
-// //int a = int.Parse(null);
 
+// // // // // Console.WriteLine($"Quantidad em estoque: {quantidadeEmEstoque}");
+// // // // // Console.WriteLine($"Quantidad compra: {quantidadeCompra}");
+// // // // // Console.WriteLine($"É possível realizar a venda? {possivelVenda}");
 
+// // // // // if (quantidadeCompra == 0)
+// // // // // {
+// // // // //     Console.WriteLine("Venda inválida");
+// // // // // }
 
-// //Console.WriteLine(a);
-// // int a = 10 ;
-// // int b = 20;
+// // // // // else if (possivelVenda)
+// // // // // {
+// // // // //     Console.WriteLine("Venda realizada");
 
-// // int c = a+b;
-// // //c = c + 5;
-// // c += 5;
-// // Console.WriteLine(c);
+// // // // // }
+// // // // // else
+// // // // // {
+// // // // //     Console.WriteLine("Desculpe, não temos a quantidade desejada em estoque");
+// // // // // }
 
 
-// // string apresentacao = "Olá, seja bem vindo";
-// // int quantidade = 1;
-// // quantidade = 10;
-// // const double altura = 1.80;
 
-// // decimal preco = 1.80M;
 
-// // bool condicao = true;
 
-// // Console.WriteLine("Valor da variável apresentação:" + apresentacao);
-// // Console.WriteLine("Valor da variável quantidade: " + quantidade);
-// // Console.WriteLine("Valor da variável altura: " + altura.ToString("0.00"));
+// // // // // // double a  = 4 /(2 + 2);
 
+// // // // // // Console.WriteLine(a);
 
-// // Console.WriteLine("Valor da variável preço " + preco);
-// // Console.WriteLine("Valor da variável condicao: " + condicao);
 
 
-// // DateTime dataAtual = DateTime.Now.AddDays(5);
-// //Console.WriteLine(dataAtual);
-// //Console.WriteLine(dataAtual.ToString("dd/MM"));
-// //Console.WriteLine(dataAtual.ToString("dd/MM/YYYY/HH:mm:ss"));
 
 
+// // // // // // int a = int.MaxValue;
 
+// // // // // // long b = a;
 
+// // // // // // Console.WriteLine(b);
 
 
 
 
 
 
+// // // // // // int inteiro = 5;
+// // // // // // string a = inteiro.ToString();
 
+// // // // // // Console.WriteLine(a);
 
-// //  Pessoa pessoa1 = new Pessoa();
 
-// //pessoa1.Nome = "Buta";
-// //pessoa1.Idade = 26;
-// //pessoa1.Apresentar();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // // // // // Cast - Casting 
+
+// // // // // //int a =  Convert.ToInt32(null);
+
+
+// // // // // //int a = int.Parse(null);
+
+
+
+// // // // // //Console.WriteLine(a);
+// // // // // // int a = 10 ;
+// // // // // // int b = 20;
+
+// // // // // // int c = a+b;
+// // // // // // //c = c + 5;
+// // // // // // c += 5;
+// // // // // // Console.WriteLine(c);
+
+
+// // // // // // string apresentacao = "Olá, seja bem vindo";
+// // // // // // int quantidade = 1;
+// // // // // // quantidade = 10;
+// // // // // // const double altura = 1.80;
+
+// // // // // // decimal preco = 1.80M;
+
+// // // // // // bool condicao = true;
+
+// // // // // // Console.WriteLine("Valor da variável apresentação:" + apresentacao);
+// // // // // // Console.WriteLine("Valor da variável quantidade: " + quantidade);
+// // // // // // Console.WriteLine("Valor da variável altura: " + altura.ToString("0.00"));
+
+
+// // // // // // Console.WriteLine("Valor da variável preço " + preco);
+// // // // // // Console.WriteLine("Valor da variável condicao: " + condicao);
+
+
+// // // // // // DateTime dataAtual = DateTime.Now.AddDays(5);
+// // // // // //Console.WriteLine(dataAtual);
+// // // // // //Console.WriteLine(dataAtual.ToString("dd/MM"));
+// // // // // //Console.WriteLine(dataAtual.ToString("dd/MM/YYYY/HH:mm:ss"));
+
+
+
+
+
+
+
+
+
+
+
+
+// // // // // //  Pessoa pessoa1 = new Pessoa();
+
+// // // // // //pessoa1.Nome = "Buta";
+// // // // // //pessoa1.Idade = 26;
+// // // // // //pessoa1.Apresentar();
 
